2bf2e792aea1128af99c067f5a6a3b9e0fba17b1
Cyan Lin <xusine@outlook.com>
2023-03-13 22:44:21 +0100

    make thread count variable

--- src/bin/speed_estimate.rs
+++ src/bin/speed_estimate.rs
@@ -1,15 +1,16 @@
-use chrono::Local;
 use once_cell::sync::OnceCell;
 use rand::distributions::WeightedIndex;
 use rand::prelude::*;
+use std::env;
 use std::fs;
+use std::process::exit;
 use std::sync;
 use std::thread;
+use std::time::Instant;
 
 use worm_cache::cache::parallel::{BlockState, CacheEntry, ParallelCache};
 use worm_cache::cache::CacheReturnResult;
 
-const THREAD_COUNT: usize = 8;
 const LLC_SET: usize = 64 * 1024;
 
 static SEEDS: OnceCell<Vec<u32>> = once_cell::sync::OnceCell::new();
@@ -17,7 +18,16 @@ static THREAD_BARRIER: OnceCell<sync::Barrier> = once_cell::sync::OnceCell::new(
 static CACHE: OnceCell<ParallelCache<CacheEntry>> = once_cell::sync::OnceCell::new();
 
 fn main() {
-    let mut f = fs::read_to_string("./llc_counter.log").unwrap();
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <thread_number>", args[0]);
+        exit(-1)
+    }
+
+    let thread_count = args[1].parse::<usize>().unwrap();
+
+    let f = fs::read_to_string("./llc_counter.log").unwrap();
+
     let seeds: Vec<_> = f
         .split(" ")
         .filter_map(|s| {
@@ -31,11 +41,11 @@ fn main() {
     println!("Seed count: {}", SEEDS.get().unwrap().len());
 
     THREAD_BARRIER
-        .set(sync::Barrier::new(THREAD_COUNT + 1))
+        .set(sync::Barrier::new(thread_count))
         .unwrap();
     CACHE.set(ParallelCache::new(LLC_SET, 8)).unwrap();
 
-    let threads_handlers: Vec<_> = (0..THREAD_COUNT)
+    let threads_handlers: Vec<_> = (0..thread_count)
         .map(|_| {
             return thread::spawn(|| {
                 // step1, generate 10M requests
@@ -46,6 +56,7 @@ fn main() {
                 THREAD_BARRIER.wait();
                 let mut cnt = 0u64;
                 // now, send the request to the cache and start timing.
+                let t = Instant::now();
                 for _ in 0..2 {
                     for s in seeds.iter() {
                         match local_cache.update(*s, BlockState::Exclusive) {
@@ -57,17 +68,13 @@ fn main() {
                         }
                     }
                 }
-                println!("Misses: {}", cnt);
+                let el = t.elapsed();
+                println!("Duration: {}ms", el.as_millis());
             });
         })
         .collect();
 
-    THREAD_BARRIER.wait();
-    let before_exp = Local::now();
     threads_handlers.into_iter().for_each(|x| {
         x.join().unwrap();
     });
-    let after_exp = Local::now();
-
-    println!("Finish interval: {}", (after_exp - before_exp).num_milliseconds());
 }
