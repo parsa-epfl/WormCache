fea0d1ac5251a84ad4fa06049d12991de7a815d1
Cyan Lin <xusine@outlook.com>
2023-05-21 00:27:26 +0200

    feat: add serialized function

--- src/plugin/set_contention_analysis.rs
+++ src/plugin/set_contention_analysis.rs
@@ -6,6 +6,8 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::prelude::*;
 
+use serde_json;
+
 use super::PerInstructionInstrumentation;
 use super::QEMUMemoryInfo;
 use super::QEMUPluginBasicBlock;
@@ -15,7 +17,7 @@ pub struct LLCSetAccessDistributionPlugin {
     llc_counter: Vec<HashMap<usize, usize>>,
 }
 
-const LLC_SET: usize = 1024 * 64;
+const LLC_SET: usize = 1024 * 1024;
 
 impl LLCSetAccessDistributionPlugin {
     pub fn new() -> Self {
@@ -29,8 +31,13 @@ impl LLCSetAccessDistributionPlugin {
         };
     }
 
-    pub fn increaseLLCCounter(&mut self, block_id: usize) {
-
+    pub fn increase_llc_counter(&mut self, block_id: usize) {
+        match self.llc_counter[block_id % LLC_SET].get_mut(&block_id) {
+            Some(freq) => *freq += 1,
+            None => {
+                self.llc_counter[block_id % LLC_SET].insert(block_id, 1);
+            }
+        };
     }
 }
 
@@ -48,19 +55,19 @@ unsafe impl QEMUPlugin for LLCSetAccessDistributionPlugin {
                 if idx == 0 {
                     return PerInstructionInstrumentation {
                         instruction_execution: Some(pc as *mut std::ffi::c_void),
-                        memory_access: Some(pc_as_param)
+                        memory_access: Some(pc_as_param),
                     };
                 } else {
                     if (pc % 64) == 0 {
                         // only insert when there it crosses the cache line
                         return PerInstructionInstrumentation {
                             instruction_execution: Some(pc as *mut std::ffi::c_void),
-                            memory_access: Some(pc_as_param)
+                            memory_access: Some(pc_as_param),
                         };
                     } else {
                         return PerInstructionInstrumentation {
                             instruction_execution: None,
-                            memory_access: Some(pc_as_param)
+                            memory_access: Some(pc_as_param),
                         };
                     }
                 }
@@ -78,16 +85,16 @@ unsafe impl QEMUPlugin for LLCSetAccessDistributionPlugin {
 
         match self.private_cache.update(block_id, false) {
             CacheReturnResult::Miss => {
-                self.increaseLLCCounter(block_id);
+                self.increase_llc_counter(block_id);
             }
             CacheReturnResult::Hit => {}
             CacheReturnResult::MissWithEviction(eviction_block_id) => {
-                self.increaseLLCCounter(block_id);
-                self.increaseLLCCounter(eviction_block_id);
+                self.increase_llc_counter(block_id);
+                self.increase_llc_counter(eviction_block_id);
             }
             CacheReturnResult::MissWithWriteBack(write_back_block_id) => {
-                self.increaseLLCCounter(block_id);
-                self.increaseLLCCounter(write_back_block_id);
+                self.increase_llc_counter(block_id);
+                self.increase_llc_counter(write_back_block_id);
             }
         }
     }
@@ -112,25 +119,24 @@ unsafe impl QEMUPlugin for LLCSetAccessDistributionPlugin {
 
         match self.private_cache.update(block_id, false) {
             CacheReturnResult::Miss => {
-                self.increaseLLCCounter(block_id);
+                self.increase_llc_counter(block_id);
             }
             CacheReturnResult::Hit => {}
             CacheReturnResult::MissWithEviction(eviction_block_id) => {
-                self.increaseLLCCounter(block_id);
-                self.increaseLLCCounter(eviction_block_id);
+                self.increase_llc_counter(block_id);
+                self.increase_llc_counter(eviction_block_id);
             }
             CacheReturnResult::MissWithWriteBack(write_back_block_id) => {
-                self.increaseLLCCounter(block_id);
-                self.increaseLLCCounter(write_back_block_id);
+                self.increase_llc_counter(block_id);
+                self.increase_llc_counter(write_back_block_id);
             }
         }
     }
 
     unsafe fn on_qemu_exit(&mut self) {
         // now, all the statistically saved.
-        let mut llc_counters = fs::File::create("llc_counter.log").unwrap();
-        for set_contention in self.llc_counter.iter() {
-        }
+        let mut llc_counters = fs::File::create("llc_counter.json").unwrap();
+        llc_counters.write(serde_json::to_string_pretty(&self.llc_counter).unwrap().as_bytes()).unwrap();
         llc_counters.flush().unwrap();
     }
 }
