b5a539cff6423835b84fbadf943cafc8ba124c6c
cyan.lin <xusine@outlook.com>
2023-10-18 21:05:54 +0200

    print log

--- src/components/virtual_time/mod.rs
+++ src/components/virtual_time/mod.rs
@@ -2,7 +2,7 @@ mod icount;
 mod vtime;
 
 use once_cell::sync::Lazy;
-use std::sync::Mutex;
+use std::{io::Write, sync::Mutex};
 
 use crate::qemu_api;
 
@@ -11,49 +11,67 @@ static TIME_PLUGIN: Lazy<Mutex<vtime::VirtualTimeContext>> =
 
 static ICOUNT_PLUGIN: Lazy<icount::ICountPlugin> = Lazy::new(|| icount::ICountPlugin::new());
 
-
 #[no_mangle]
 unsafe extern "C" fn calculate_virtual_time() -> i64 {
-    return TIME_PLUGIN.lock().unwrap().calculate_virtual_time(&ICOUNT_PLUGIN);
+    return TIME_PLUGIN
+        .lock()
+        .unwrap()
+        .calculate_virtual_time(&ICOUNT_PLUGIN);
 }
 
-#[no_mangle]
-unsafe extern "C" fn icount_incrementing(
-    vcpu_idx: u32,
-    user_data: *mut std::ffi::c_void,
-) {
-    let core_id = vcpu_idx as u8;
-    let count = user_data as usize;
-    ICOUNT_PLUGIN.increase_icount(core_id, count);
+// memory ts for print the log.
+fn get_memory_ts() -> u128 {
+    return std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_nanos() as u128;
 }
 
 // For each component, you should provide the following functions.
-// 1. init function. 
+// 1. init function.
 #[inline]
 pub unsafe fn init() {
     qemu_api::qemu_plugin_register_virtual_time_cb(Some(calculate_virtual_time));
+    std::thread::spawn(|| {
+        // open a csv file to store the icounts.
+        let mut file = std::fs::File::create("cache-icount.csv").unwrap();
+        // write the header.
+        // file.write_fmt(format_args!("ts")).unwrap();
+        // for i in 0..CORE_COUNT {
+        //     file.write_fmt(format_args!(",core{}", i)).unwrap();
+        // }
+        // file.write_fmt(format_args!("\n")).unwrap();
+        let mut head = vec!["ts".to_string()];
+        for i in 0..crate::CORE_COUNT {
+            head.push(format!("core{}", i));
+        }
+
+        file.write_fmt(format_args!("{}\n", head.join(",")))
+            .unwrap();
+
+        loop {
+            let icounts = ICOUNT_PLUGIN.get_icounts();
+            let mut lines = vec![];
+            lines.push(format!("{}", get_memory_ts()));
+            for i in 0..crate::CORE_COUNT {
+                lines.push(format!("{}", icounts[i]));
+            }
+            file.write_fmt(format_args!("{}\n", lines.join(",")))
+                .unwrap();
+            std::thread::sleep(std::time::Duration::from_secs(10));
+        }
+    });
 }
 
-
-// 2. translation function. 
+// 2. translation function.
 #[inline]
-pub fn on_instruction_cacheline_touched(
-    vcpu_idx: u32,
-    context: &crate::PluginFetchBlockContext
-) {
+pub fn on_instruction_cacheline_touched(vcpu_idx: u32, context: &crate::PluginFetchBlockContext) {
     ICOUNT_PLUGIN.increase_icount(vcpu_idx as u8, context.size);
 }
 
 // 3. on memory access
 #[inline]
-pub fn on_data_cacheline_touched(
-    _: u32,
-    _: usize,
-    _: usize,
-    _: bool
-) {
-    
-}
+pub fn on_data_cacheline_touched(_: u32, _: usize, _: usize, _: bool) {}
 
 // 3. the function to export checkpoint. This function is guaranteed to be serial.
 #[inline]
