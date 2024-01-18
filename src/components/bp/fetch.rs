cdad082f7749306d50a1dc40526cd63df4b53d63
cyan.lin <xusine@outlook.com>
2024-01-18 17:23:00 +0100

    fix: constant

--- src/components/bp/fetch.rs
+++ src/components/bp/fetch.rs
@@ -6,7 +6,7 @@ mod tage;
 
 use serde::Serialize;
 
-use crate::{parameter::BP_GSHARE_SET, BP_RAS_COUNT, parameter::CORE_COUNT};
+use crate::{parameter::BP_GSHARE_SET, parameter::BP_RAS_COUNT, parameter::CORE_COUNT};
 
 use super::BranchResolveFlag;
 
