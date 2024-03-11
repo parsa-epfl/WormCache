fc6c507465e2452b181a02c2da072de4eea1ce22
cyan.lin <xusine@outlook.com>
2024-03-11 19:44:26 +0100

    chore: remove coherence protocol code

--- src/components/memory_delayed/coherence_protocol.rs
+++
@@ -1,8 +0,0 @@
-
-trait CoherenceProtocol {
-    fn is_hit(&self);
-}
-
-// Each coherence protocol has two parts:
-// - The state machine (next state) of the cache line.
-// - The state machine of the directory entry.
--- src/components/memory_delayed/mod.rs
+++ src/components/memory_delayed/mod.rs
@@ -18,7 +18,6 @@ mod hierarchy;
 mod private_cache;
 pub mod shared_cache;
 pub mod statistics;
-mod coherence_protocol;
 
 static mut PLUGIN: Lazy<hierarchy::PluginDelayedMemoryHierarchy> =
     Lazy::new(|| hierarchy::PluginDelayedMemoryHierarchy::new());
