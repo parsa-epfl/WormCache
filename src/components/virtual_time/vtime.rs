7742f34d42cb97df51400d4d5213f5c84ad9e3f3
cyan.lin <xusine@outlook.com>
2024-04-08 21:26:45 +0200

    feat: decouple the user and kernel i count

--- src/components/virtual_time/vtime.rs
+++ src/components/virtual_time/vtime.rs
@@ -2,15 +2,13 @@ use crate::parameter as param;
 use crate::qemu_api::qemu_plugin_cpu_is_tick_enabled;
 use crate::qemu_api::qemu_plugin_get_snapshot_cpu_clock;
 
-use param::CORE_COUNT;
-
 use std::time::SystemTime;
 
 use super::icount::ICountPlugin;
 
 pub struct VirtualTimeContext {
     last_real_time: i128,
-    last_icounts: [u64; CORE_COUNT],
+    last_icount: u64,
     advanced_vclock: i64,
 }
 
@@ -18,7 +16,7 @@ impl VirtualTimeContext {
     pub fn new() -> Self {
         return Self {
             last_real_time: 0,
-            last_icounts: [0; CORE_COUNT],
+            last_icount: 0,
             advanced_vclock: 0,
         };
     }
@@ -31,22 +29,16 @@ impl VirtualTimeContext {
             .as_nanos() as i128;
 
         // 2. get the icounts from each core
-        let icounts = icount.get_icounts();
+        let icounts = icount.get_total_icounts_of_core(0);
 
         // 3. calculate the potential update
         unsafe {
             if qemu_plugin_cpu_is_tick_enabled() {
                 // 3.1 calculate the difference icounts and find the maximum.
-                let mut max_icounts = 0;
-                for i in 0..CORE_COUNT {
-                    let diff = icounts[i] - self.last_icounts[i];
-                    if diff > max_icounts {
-                        max_icounts = diff;
-                    }
-                }
+                let mut icount_diff = icounts - self.last_icount;
 
                 // 3.2 if the maximum is zero, we use the difference of the real time.
-                let advanced_vtime = if max_icounts == 0 {
+                let advanced_vtime = if icount_diff == 0 {
                     if self.last_real_time == 0 {
                         // the first time this function is called. We should ignore it.
                         0
@@ -54,7 +46,7 @@ impl VirtualTimeContext {
                         (real_time - self.last_real_time) as i64
                     }
                 } else {
-                    max_icounts as i64
+                    icount_diff as i64
                 };
 
                 // 3.3 update the advanced vclock
@@ -63,7 +55,7 @@ impl VirtualTimeContext {
         }
 
         // 4. update the context with the new icounts and the real time.
-        self.last_icounts.copy_from_slice(&icounts);
+        self.last_icount = icounts;
         self.last_real_time = real_time;
 
         // 5. return the calculated virtual time
@@ -101,7 +93,7 @@ impl VirtualTimeContext {
 
     pub fn reset(&mut self) {
         self.last_real_time = 0;
-        self.last_icounts = [0; CORE_COUNT];
+        self.last_icount = 0;
         self.advanced_vclock = 0;
     }
 }
