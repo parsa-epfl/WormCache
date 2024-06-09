03fd01151e8a12edacb92f79238acac2ff9f086f
cyan.lin <xusine@outlook.com>
2024-06-09 23:25:15 +0200

    feat: Use monotonic clock to drive the system time

--- src/components/virtual_time/vtime.rs
+++ src/components/virtual_time/vtime.rs
@@ -2,10 +2,8 @@ use crate::parameter as param;
 use crate::qemu_api::qemu_plugin_cpu_is_tick_enabled;
 use crate::qemu_api::qemu_plugin_get_snapshot_cpu_clock;
 
-use std::time::SystemTime;
-
 pub struct VirtualTimeContext {
-    last_real_time: i128,
+    last_real_time: u64,
     advanced_vclock: i64,
     time_scaling_factor: u64,
 }
@@ -28,17 +26,19 @@ impl VirtualTimeContext {
 
     pub fn calculate_cpu_clock(&mut self) -> i64 {
         // 1. get real timestamp in nanosecond
-        let real_time = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos() as i128;
+        // let real_time = SystemTime::now()
+        //     .duration_since(SystemTime::UNIX_EPOCH)
+        //     .unwrap()
+        //     .as_nanos() as i128;
+
+        let real_time = crate::util::get_monotonic_ts();
 
         // 2. calculate the potential update
         unsafe {
             if qemu_plugin_cpu_is_tick_enabled() && self.last_real_time != 0 {
                 // 3.2 if the maximum is zero, we use the difference of the real time.
                 let advanced_vtime =
-                    ((real_time - self.last_real_time) / self.time_scaling_factor as i128) as i64;
+                    ((real_time - self.last_real_time) / self.time_scaling_factor) as i64;
 
                 // 3.3 update the advanced vclock
                 self.advanced_vclock += advanced_vtime;
@@ -58,10 +58,7 @@ impl VirtualTimeContext {
 
     #[allow(dead_code)]
     pub fn calculate_cpu_clock_with_10x_slowdown_from_realtime(&mut self) -> i64 {
-        let real_time = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_nanos() as i128;
+        let real_time = crate::util::get_monotonic_ts();
 
         unsafe {
             if qemu_plugin_cpu_is_tick_enabled() && self.last_real_time != 0 {
