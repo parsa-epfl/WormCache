e55dc17dc2006ffdadd361091babf0f845c38fd7
cyan.lin <xusine@outlook.com>
2024-06-28 01:31:08 +0200

    add tracer

--- src/components/trace/mod.rs
+++ src/components/trace/mod.rs
@@ -2,47 +2,35 @@ use std::{
     ffi,
     fs::File,
     io::{BufWriter, Write},
-    sync::Mutex,
+    process::exit,
 };
 
-use rustc_hash::FxHashMap as HashMap;
+use zstd::Encoder;
 
-use once_cell::sync::Lazy;
+static mut TRACE_FILE: *mut Encoder<BufWriter<File>> = std::ptr::null_mut();
+
+static mut C0_COUNTER: std::sync::atomic::AtomicU64 = std::sync::atomic::AtomicU64::new(0);
 
 use crate::qemu_api;
 
-use crate::util::get_monotonic_ts;
+unsafe extern "C" fn vcpu_insn_exec(vcpu_idx: u32, host_va: *mut ffi::c_void) {
+    if !vcpu_idx == 0 {
+        return;
+    }
 
-static TRACE_FILE: Lazy<Mutex<BufWriter<File>>> = Lazy::new(|| {
-    // open a file to store the trace.
-    let file = std::fs::File::create("trace.trace").unwrap();
-    let b = BufWriter::with_capacity(64 * 1024 * 1024, file);
-    Mutex::new(b)
-});
+    let host_va_u64 = host_va as u64;
+    let pc = qemu_api::qemu_plugin_read_pc_vpn() << 12 | host_va_u64 & 0xfff;
+    let instruction_literal = unsafe { *(host_va as *mut u32) };
 
-#[cfg(target_pointer_width = "64")]
-#[derive(Debug)]
-pub struct PluginFetchBlockContext {
-    /// The virtual address of the first instruction.
-    pub va: usize,
-    /// The physical address of the first instruction.
-    pub pa: usize,
-    /// The number of instructions in this fetch block.
-    pub size: usize,
-}
+    // write the instruction to the trace file.
+    unsafe { writeln!(*TRACE_FILE, "{:x},{:x}", pc, instruction_literal).unwrap() };
 
-static FETCH_BLOCK_CONTEXT_MAP: Lazy<Mutex<HashMap<usize, Box<PluginFetchBlockContext>>>> =
-    Lazy::new(|| Mutex::new(HashMap::default()));
-
-unsafe extern "C" fn vcpu_insn_exec(vcpu_idx: u32, size: *mut ffi::c_void) {
-    let context = &*(size as *mut PluginFetchBlockContext);
-
-    let mut buffer = [0u8; 18];
-    buffer[0..8].copy_from_slice(&context.pa.to_le_bytes());
-    buffer[8..16].copy_from_slice(&get_monotonic_ts().to_le_bytes());
-    buffer[16] = 0;
-    buffer[17] = vcpu_idx as u8;
-    TRACE_FILE.lock().unwrap().write_all(&buffer).unwrap();
+    // increment the counter.
+    unsafe {
+        if C0_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed) == 20000000 {
+            exit(0);
+        }
+    };
 }
 
 unsafe extern "C" fn vcpu_mem_access(
@@ -57,13 +45,6 @@ unsafe extern "C" fn vcpu_mem_access(
     if !is_device {
         let is_store = qemu_api::qemu_plugin_mem_is_store(info);
         let paddr = qemu_api::qemu_plugin_hwaddr_phys_addr(hw_handler) as usize;
-
-        let mut buffer = [0u8; 18];
-        buffer[0..8].copy_from_slice(&paddr.to_le_bytes());
-        buffer[8..16].copy_from_slice(&get_monotonic_ts().to_le_bytes());
-        buffer[16] = if is_store { 2 } else { 1 };
-        buffer[17] = cpu_idx as u8;
-        TRACE_FILE.lock().unwrap().write_all(&buffer).unwrap();
     } else {
         // TODO: check the I/O event
     }
@@ -75,15 +56,19 @@ pub struct TracePlugin {}
 impl super::Plugin for TracePlugin {
     #[inline]
     fn init() {
-        // make sure the file is initialized.
-        TRACE_FILE.lock().unwrap().flush().unwrap();
-        println!("Trace plugin initialized.");
+        unsafe {
+            TRACE_FILE = Box::into_raw(Box::new(
+                Encoder::new(
+                    BufWriter::new(File::create("worm_cache.c0.trace.zst").unwrap()),
+                    4,
+                )
+                .unwrap(),
+            ));
+        }
     }
 
     #[inline]
-    fn dump_snapshot(_: &str) {
-        TRACE_FILE.lock().unwrap().flush().unwrap();
-    }
+    fn dump_snapshot(_: &str) {}
 
     unsafe fn on_translation(tb: *mut crate::qemu_api::qemu_plugin_tb) {
         let n_instruction = qemu_api::qemu_plugin_tb_n_insns(tb);
@@ -92,59 +77,23 @@ impl super::Plugin for TracePlugin {
             return;
         }
 
-        let mut block_id = vec![];
-        for i in 0..n_instruction {
-            let inst = qemu_api::qemu_plugin_tb_get_insn(tb, i);
-            block_id.push(
-                qemu_api::qemu_plugin_insn_haddr(inst) as usize
-                    >> crate::parameter::CACHE_LINE_SIZE.trailing_zeros(),
-            );
-        }
-
-        let fb_info = crate::util::find_fetch_block_from_block_id_sequence(block_id);
-
-        // bind the instruction call back.
-        for (idx, size) in fb_info.into_iter() {
-            let i = qemu_api::qemu_plugin_tb_get_insn(tb, idx);
-            // register the metadata first.
-            let mut map = FETCH_BLOCK_CONTEXT_MAP.lock().unwrap();
-            let ctx_ptr: *const PluginFetchBlockContext = match map.get_mut(&idx) {
-                Some(old_ctx) => {
-                    old_ctx.va = qemu_api::qemu_plugin_insn_vaddr(i) as usize;
-                    old_ctx.pa = qemu_api::qemu_plugin_insn_haddr(i) as usize;
-                    old_ctx.size = size;
-                    let res: *const PluginFetchBlockContext = &**old_ctx;
-                    res
-                }
-                None => {
-                    let ctx = Box::new(PluginFetchBlockContext {
-                        va: qemu_api::qemu_plugin_insn_vaddr(i) as usize,
-                        pa: qemu_api::qemu_plugin_insn_haddr(i) as usize,
-                        size,
-                    });
-                    let res: *const PluginFetchBlockContext = &*ctx;
-                    map.insert(idx, ctx);
-                    res
-                }
-            };
-            qemu_api::qemu_plugin_register_vcpu_insn_exec_cb(
-                i,
-                Some(vcpu_insn_exec),
-                qemu_api::qemu_plugin_cb_flags_QEMU_PLUGIN_CB_NO_REGS,
-                ctx_ptr as *mut ffi::c_void,
-            );
-        }
-
         // bind the memory callback.
         for i in 0..n_instruction {
             let inst = qemu_api::qemu_plugin_tb_get_insn(tb, i);
-            qemu_api::qemu_plugin_register_vcpu_mem_cb(
+            let host_va_instruction = qemu_api::qemu_plugin_insn_haddr(inst);
+            qemu_api::qemu_plugin_register_vcpu_insn_exec_cb(
                 inst,
-                Some(vcpu_mem_access),
+                Some(vcpu_insn_exec),
                 qemu_api::qemu_plugin_cb_flags_QEMU_PLUGIN_CB_NO_REGS,
-                qemu_api::qemu_plugin_mem_rw_QEMU_PLUGIN_MEM_RW,
-                std::ptr::null_mut(),
+                host_va_instruction,
             );
+            // qemu_api::qemu_plugin_register_vcpu_mem_cb(
+            //     inst,
+            //     Some(vcpu_mem_access),
+            //     qemu_api::qemu_plugin_cb_flags_QEMU_PLUGIN_CB_NO_REGS,
+            //     qemu_api::qemu_plugin_mem_rw_QEMU_PLUGIN_MEM_RW,
+            //     std::ptr::null_mut(),
+            // );
         }
     }
 }
