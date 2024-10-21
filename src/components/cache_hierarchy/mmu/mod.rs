fb17714d9b329d255e25f8efb7db02473371919d
Cyan Lin <xusine@outlook.com>
2024-10-21 15:45:34 +0200

    fix: Get ASID from proper register file  (#47)
    * fix: Read ASID from the proper base register
    
    * feat: Add assertion for checking global TLB entry

--- src/components/cache_hierarchy/mmu/mod.rs
+++ src/components/cache_hierarchy/mmu/mod.rs
@@ -34,15 +34,15 @@
 
 pub mod tlb;
 
-use crate::components::debug::statistics::EventType;
-use crate::{arch, components::debug::statistics::Statistics};
 use crate::arch::aarch64::ptw;
+use crate::components::debug::statistics::EventType;
 use crate::qemu_api;
+use crate::{arch, components::debug::statistics::Statistics};
 
 use rustc_hash::FxHashMap as HashMap;
 use serde::{Deserialize, Serialize};
 use std::ffi::c_void;
-use tlb::TLB;
+use tlb::{AddressSpaceID, TLB};
 
 pub trait AbstractMMU {
     fn new() -> Self;
@@ -55,7 +55,6 @@ pub trait AbstractMMU {
     ) -> MMUTranslationResult;
 
     // Currently, this interface is for debugging. It reuses QEMU's PTW result.
-    fn refill_4k_tlb(&mut self, vpn: u64, ppn: u64, ts: u64, is_instruction: bool);
     fn lookup(&mut self, vpn: u64, ts: u64, is_instruction: bool) -> Option<u64>;
 
     fn serialize(&self) -> serde_json::Value;
@@ -77,7 +76,6 @@ impl AbstractMMU for NoMMU {
     ) -> MMUTranslationResult {
         MMUTranslationResult::Hit(va)
     }
-    fn refill_4k_tlb(&mut self, _: u64, _: u64, _: u64, _: bool) {}
     fn lookup(&mut self, _: u64, _: u64, _: bool) -> Option<u64> {
         None
     }
@@ -105,8 +103,8 @@ pub struct MemoryManagementUnit<
     itlb: TLB<I_T_SETS, I_T_ASSO>,
     dtlb: TLB<D_T_SETS, D_T_ASSO>,
     stlb: TLB<S_T_SETS, S_T_ASSO>,
-    htbl_2mb: HashMap<u64, u64>, // Currently, we just use a simple hashmap to store the 2MB page table.
-    htlb_1gb: HashMap<u64, u64>, // Same to the 2MB page table.
+    htbl_2mb: HashMap<u64, (AddressSpaceID, u64)>, // Currently, we just use a simple hashmap to store the 2MB page table.
+    htlb_1gb: HashMap<u64, (AddressSpaceID, u64)>, // Same to the 2MB page table.
     last_ttbr: u64,
     arch: std::marker::PhantomData<ARCH>,
     // other MMU caches can be also added here as well.
@@ -126,6 +124,48 @@ fn paddr_reader(addr: u64) -> u64 {
         qemu_api::qemu_plugin_read_physical_memory(addr, 8, &mut buf as *mut u64 as *mut c_void);
     }
     buf
+}
+
+impl<
+        const I_T_A: usize,
+        const I_T_S: usize,
+        const D_T_A: usize,
+        const D_T_S: usize,
+        const S_ENABLED: bool,
+        const S_T_A: usize,
+        const S_T_S: usize,
+        const NO_HUGE_PAGE: bool,
+    >
+    MemoryManagementUnit<
+        arch::AArch64,
+        I_T_A,
+        I_T_S,
+        D_T_A,
+        D_T_S,
+        S_ENABLED,
+        S_T_A,
+        S_T_S,
+        NO_HUGE_PAGE,
+    >
+{
+    fn refill_4k_tlb(
+        &mut self,
+        vpn: u64,
+        asid: AddressSpaceID,
+        ppn: u64,
+        ts: u64,
+        is_instruction: bool,
+    ) {
+        if is_instruction {
+            self.itlb.insert(vpn, asid.clone(), ppn, ts, is_instruction);
+        } else {
+            self.dtlb.insert(vpn, asid.clone(), ppn, ts, is_instruction);
+        }
+
+        if S_ENABLED {
+            self.stlb.insert(vpn, asid, ppn, ts, is_instruction);
+        }
+    }
 }
 
 impl<
@@ -169,23 +209,18 @@ impl<
         ts: u64,
         is_instruction: bool,
     ) -> MMUTranslationResult {
-        let is_kernel = (va >> 63) != 0;
-
-        if is_kernel {
-            self.last_ttbr = u64::MAX;
-        } else if self.last_ttbr == u64::MAX {
-            self.last_ttbr = unsafe { qemu_api::qemu_plugin_read_ttbr_el1(0) };
-        }
-
-        let asid = if is_kernel {
-            0xffff_u16
-        } else {
-            // TODO: The asid may come from a different register. This requires further investigation.
-            (self.last_ttbr >> 48) as u16
-        };
+        let tcr = unsafe { qemu_api::qemu_plugin_read_tcr_el1() };
+        // How do we decide whether this is a kernel space or a user space?
+        // I have to read the two granules.
+        let which_ttbr_for_asid = if tcr >> 22 & 0b1 == 1 { 1 } else { 0 };
 
         // First, we try 4KB page.
         let vpn = va >> 12;
+        let asid = tlb::AddressSpaceID::NonGlobal(unsafe {
+            (qemu_api::qemu_plugin_read_ttbr_el1(which_ttbr_for_asid) >> 48) as u16
+        }); // We will start with a non-global ASID. It can still match the global ASID.
+
+        let is_kernel = (vpn >> 51) == 1;
 
         Statistics::global_record(core_id, EventType::TLBAccess, is_kernel);
 
@@ -219,61 +254,90 @@ impl<
         if !NO_HUGE_PAGE {
             // Then, we try 2MB page.
             let vpn_2mb = vpn >> 9;
-            let key_2mb = (asid as u64) << 48 | vpn_2mb;
-            if let Some(ppn) = self.htbl_2mb.get(&key_2mb) {
-                let pa = ppn << 21 | (va & 0x1fffff);
+            if let Some(ppn) = self.htbl_2mb.get(&vpn_2mb) {
+                if ppn.0.check(&asid) {
+                    let pa = ppn.1 << 21 | (va & 0x1fffff);
 
-                Statistics::global_record(core_id, EventType::HugeTLBHit, is_kernel);
+                    Statistics::global_record(core_id, EventType::HugeTLBHit, is_kernel);
 
-                if is_instruction {
-                    Statistics::global_record(
-                        core_id,
-                        EventType::HugeTLBHitDueToInstruction,
-                        is_kernel,
-                    );
-                } else {
-                    Statistics::global_record(core_id, EventType::HugeTLBHitDueToData, is_kernel);
+                    if is_instruction {
+                        Statistics::global_record(
+                            core_id,
+                            EventType::HugeTLBHitDueToInstruction,
+                            is_kernel,
+                        );
+                    } else {
+                        Statistics::global_record(
+                            core_id,
+                            EventType::HugeTLBHitDueToData,
+                            is_kernel,
+                        );
+                    }
+
+                    return MMUTranslationResult::Hit(pa);
                 }
-
-                return MMUTranslationResult::Hit(pa);
             }
 
             // Then, we try 1GB page.
             let vpn_1gb = vpn >> 18;
-            let key_1gb = (asid as u64) << 48 | vpn_1gb;
-            if let Some(ppn) = self.htlb_1gb.get(&key_1gb) {
-                Statistics::global_record(core_id, EventType::HugeTLBHit, is_kernel);
+            if let Some(ppn) = self.htlb_1gb.get(&vpn_1gb) {
+                if ppn.0.check(&asid) {
+                    Statistics::global_record(core_id, EventType::HugeTLBHit, is_kernel);
 
-                if is_instruction {
-                    Statistics::global_record(
-                        core_id,
-                        EventType::HugeTLBHitDueToInstruction,
-                        is_kernel,
-                    );
-                } else {
-                    Statistics::global_record(core_id, EventType::HugeTLBHitDueToData, is_kernel);
+                    if is_instruction {
+                        Statistics::global_record(
+                            core_id,
+                            EventType::HugeTLBHitDueToInstruction,
+                            is_kernel,
+                        );
+                    } else {
+                        Statistics::global_record(
+                            core_id,
+                            EventType::HugeTLBHitDueToData,
+                            is_kernel,
+                        );
+                    }
+
+                    let pa = ppn.1 << 30 | (va & 0x3fffffff);
+                    return MMUTranslationResult::Hit(pa);
                 }
-
-                let pa = ppn << 30 | (va & 0x3fffffff);
-                return MMUTranslationResult::Hit(pa);
             }
 
-            let ptw_result = unsafe {
-                let tcr = qemu_api::qemu_plugin_read_tcr_el1();
-                let ttbr = qemu_api::qemu_plugin_read_ttbr_el1(if is_kernel { 1 } else { 0 });
+            let ptw_result = {
+                let t1_size = 64 - ((tcr >> 16) & 0b111111);
+                let t0_size = 64 - (tcr & 0b111111);
+
+                assert!(t0_size == 48); // the lower 48-bit VA are used for translation.
+                assert!(t1_size == 48); // the OS should take over all spaces.
+
+                let which_ttbr_for_base = if va < (1 << 48) { 0 } else { 1 };
+                let ttbr = unsafe { qemu_api::qemu_plugin_read_ttbr_el1(which_ttbr_for_base) };
                 ptw(ttbr, tcr, vpn << 12, paddr_reader)
             };
+
+            let asid = if ptw_result.is_global {
+                AddressSpaceID::Global
+            } else {
+                asid
+            };
+
+            if is_kernel {
+                // kernel space has to be global address.
+                assert!(matches!(asid, AddressSpaceID::Global));
+            }
 
             // based on the ptw_result, we refill each TLB correspondingly.
             match ptw_result.page_size {
                 arch::aarch64::PageSize::_4KB => {
-                    self.refill_4k_tlb(vpn, ptw_result.paddr >> 12, ts, is_instruction);
+                    self.refill_4k_tlb(vpn, asid, ptw_result.paddr >> 12, ts, is_instruction);
                 }
                 arch::aarch64::PageSize::_2MB => {
-                    self.htbl_2mb.insert(key_2mb, ptw_result.paddr >> 21);
+                    self.htbl_2mb
+                        .insert(vpn_2mb, (asid, ptw_result.paddr >> 21));
                 }
                 arch::aarch64::PageSize::_1GB => {
-                    self.htlb_1gb.insert(key_1gb, ptw_result.paddr >> 30);
+                    self.htlb_1gb
+                        .insert(vpn_1gb, (asid, ptw_result.paddr >> 30));
                 }
             }
 
@@ -283,63 +347,48 @@ impl<
                 MMUTranslationResult::MissNotCacheable(ptw_result.paddr)
             }
         } else {
-            let ptw_result = unsafe {
-                let tcr = qemu_api::qemu_plugin_read_tcr_el1();
-                let ttbr = qemu_api::qemu_plugin_read_ttbr_el1(if is_kernel { 1 } else { 0 });
+            let ptw_result = {
+                let t1_size = 64 - ((tcr >> 16) & 0b111111);
+                let t0_size = 64 - (tcr & 0b111111);
+
+                assert!(t0_size == 48); // the lower 48-bit VA are used for translation.
+                assert!(t1_size == 48); // the OS should take over all spaces.
+
+                let which_ttbr_for_base = if va < (1 << 48) { 0 } else { 1 };
+                let ttbr = unsafe { qemu_api::qemu_plugin_read_ttbr_el1(which_ttbr_for_base) };
                 ptw(ttbr, tcr, vpn << 12, paddr_reader)
             };
 
+            let asid = if ptw_result.is_global {
+                AddressSpaceID::Global
+            } else {
+                asid.clone()
+            };
+
+            if is_kernel {
+                // kernel space has to be global address.
+                assert!(matches!(asid, AddressSpaceID::Global));
+            }
+
             // based on the ptw_result, we refill each TLB correspondingly.
-            self.refill_4k_tlb(vpn, ptw_result.paddr >> 12, ts, is_instruction);
+            self.refill_4k_tlb(vpn, asid, ptw_result.paddr >> 12, ts, is_instruction);
 
             if ptw_result.cacheable {
                 MMUTranslationResult::Miss(ptw_result.paddr, ptw_result.traces)
             } else {
                 MMUTranslationResult::MissNotCacheable(ptw_result.paddr)
             }
-        }
-    }
-
-    fn refill_4k_tlb(&mut self, vpn: u64, ppn: u64, ts: u64, is_instruction: bool) {
-        let is_kernel = (vpn >> 51) == 1;
-
-        if is_kernel {
-            self.last_ttbr = u64::MAX;
-        } else if self.last_ttbr == u64::MAX {
-            self.last_ttbr = unsafe { qemu_api::qemu_plugin_read_ttbr_el1(0) };
-        }
-
-        let asid = if is_kernel {
-            0xffff_u16
-        } else {
-            (self.last_ttbr >> 48) as u16
-        };
-
-        if is_instruction {
-            self.itlb.insert(vpn, asid, ppn, ts, is_instruction);
-        } else {
-            self.dtlb.insert(vpn, asid, ppn, ts, is_instruction);
-        }
-
-        if S_ENABLED {
-            self.stlb.insert(vpn, asid, ppn, ts, is_instruction);
         }
     }
 
     fn lookup(&mut self, vpn: u64, ts: u64, is_instruction: bool) -> Option<u64> {
-        let is_kernel = (vpn >> 51) == 1;
-
-        if is_kernel {
-            self.last_ttbr = u64::MAX;
-        } else if self.last_ttbr == u64::MAX {
-            self.last_ttbr = unsafe { qemu_api::qemu_plugin_read_ttbr_el1(0) };
-        }
-
-        let asid = if is_kernel {
-            0xffff_u16
-        } else {
-            (self.last_ttbr >> 48) as u16
-        };
+        let tcr = unsafe { qemu_api::qemu_plugin_read_tcr_el1() };
+        // How do we decide whether this is a kernel space or a user space?
+        // I have to read the two granules.
+        let which_ttbr_for_asid = if tcr >> 22 & 0b1 == 1 { 1 } else { 0 };
+        let asid = tlb::AddressSpaceID::NonGlobal(unsafe {
+            (qemu_api::qemu_plugin_read_ttbr_el1(which_ttbr_for_asid) >> 48) as u16
+        }); // We will start with a non-global ASID. It can still match the global ASID.
 
         self.stlb.lookup(vpn, asid, ts, is_instruction)
     }
