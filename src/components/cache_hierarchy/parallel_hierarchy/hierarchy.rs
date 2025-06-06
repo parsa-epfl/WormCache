9c0d2fb0eabc51565e0684851f83b68bc9d5ad94
ayanchak1508 <ayan.chakraborty@epfl.ch>
2025-06-06 11:17:04 +0200

    fix: evict

--- src/components/cache_hierarchy/parallel_hierarchy/hierarchy.rs
+++ src/components/cache_hierarchy/parallel_hierarchy/hierarchy.rs
@@ -33,7 +33,9 @@ use zstd::{Decoder, Encoder};
 
 use crate::components::cache_hierarchy::common::InfiniteDirectorySet;
 use crate::components::cache_hierarchy::mmu::AbstractMMU;
-use crate::parameter;
+use crate::components::cache_hierarchy::CacheBlockRequest;
+use crate::components::cache_hierarchy::CacheAccessType;
+use crate::parameter::{self, SMS_PREFETCHING};
 
 use crate::components::debug::statistics::{EventType, Statistics};
 
@@ -213,6 +215,25 @@ impl<
             if FILL_SCACHE_ON_PCACHE_WRITEBACK && modified.0 {
                 self.shared_cache
                     .insert(core_id, block_id, ts, modified.0, true);
+            }
+
+            if SMS_PREFETCHING {
+                let dummy_req = CacheBlockRequest{
+                    core_id: core_id,
+                    block_id,
+                    access_type: CacheAccessType::PrefetchRead, // Not needed
+                    is_os, // Not needed
+                    pc: 0, // Not needed
+                };
+                match self.agt.evict(&dummy_req) {
+                    Some(evicted_entry) => {
+                        println!("[AGT] Evicted entry into PHT: {:?}", evicted_entry);
+                        self.pht.insert(&evicted_entry, core_id as usize);
+                    }
+                    None => {
+                        println!("[AGT] No entry evicted from AGT for request: {:?}", dummy_req.block_id);
+                    },
+                }
             }
 
             if self.directory_run_gc {
--- src/parameter.rs
+++ src/parameter.rs
@@ -270,11 +270,11 @@ pub const ADJACENT_LINE_PREFETCHING: bool = false;
 /**
 * Parameters for SMS Prefetching
 */
-pub const SMS_PREFETCHING: bool = false;
+pub const SMS_PREFETCHING: bool = true;
 pub const N_ACC: usize = 64; // Number of entries in the access table.
-pub const N_FILTER: usize = 64; // Number of entries in the filter table.
-pub const N_PHT: usize = 64; // Number of entries in the pattern history table.
-pub const OFF_BITW: usize = 12;
+pub const N_FILTER: usize = 32; // Number of entries in the filter table.
+pub const N_PHT: usize = 4096; // Number of entries in the pattern history table.
+pub const OFF_BITW: usize = 11;
 pub const N_BLK: usize = 1 << OFF_BITW; // Number of blocks in the access table and filter table.
 
 /**
