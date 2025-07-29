ca7838b39bcd686cff99f9cfdede60084b500019
Ayan Chakraborty <ayan.chakraborty@epfl.ch>
2025-07-29 20:12:23 +0000

    feat:scripts

--- scripts/gen_plugin.sh
+++ scripts/gen_plugin.sh
@@ -1,74 +1,100 @@
 #!/bin/bash
 
 cd ..
-satcnt=true
-rdwr=true
-rot=true
-for pht in 16 32 64 128 256 512 1024 perfect
+
+for type in os an
 do
-    srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
-    dstfile="$(pwd)/src/parameter.rs"
-    echo "Copying ${srcfile} to ${dstfile}"
-    cp $srcfile $dstfile
+    for cnt in 1 2 4 8 16 32 64
+    do
+        for cache in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576
+        do
+            if [ "$type" = "os" ] && [ $cnt -eq 1 ]; then
+                continue
+            fi
+            srcfile="$(pwd)/src/configs/${type}_${cnt}_${cache}.rs"
+            dstfile="$(pwd)/src/parameter.rs"
+            echo "Copying ${srcfile} to ${dstfile}"
+            cp $srcfile $dstfile
 
-    echo "Building ..."
-    cargo build --release --bin worm_cache
+            echo "Building ..."
+            cargo build --release
 
-    srcfile="$(pwd)/target/release/worm_cache"
-    dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
-    cp $srcfile $dstfile
+            srcfile="$(pwd)/target/release/libworm_cache.so"
+            dstfile="$(pwd)/../bins/ws/${type}_${cnt}_${cache}.so"
+            echo "Copying ${srcfile} to ${dstfile}"
+            cp $srcfile $dstfile
+        done
+    done
 done
 
-satcnt=true
-rdwr=true
-rot=false
-for pht in 16 32 64 128 256 512 1024 perfect
-do
-    srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
-    dstfile="$(pwd)/src/parameter.rs"
-    echo "Copying ${srcfile} to ${dstfile}"
-    cp $srcfile $dstfile
+# satcnt=true
+# rdwr=true
+# rot=true
+# for pht in 16 32 64 128 256 512 1024 perfect
+# do
+#     srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
+#     dstfile="$(pwd)/src/parameter.rs"
+#     echo "Copying ${srcfile} to ${dstfile}"
+#     cp $srcfile $dstfile
 
-    echo "Building ..."
-    cargo build --release --bin worm_cache
+#     echo "Building ..."
+#     cargo build --release --bin worm_cache
 
-    srcfile="$(pwd)/target/release/worm_cache"
-    dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
-    cp $srcfile $dstfile
-done
+#     srcfile="$(pwd)/target/release/worm_cache"
+#     dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
+#     cp $srcfile $dstfile
+# done
 
-satcnt=true
-rdwr=false
-rot=false
-for pht in 16 32 64 128 256 512 1024 perfect
-do
-    srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
-    dstfile="$(pwd)/src/parameter.rs"
-    echo "Copying ${srcfile} to ${dstfile}"
-    cp $srcfile $dstfile
+# satcnt=true
+# rdwr=true
+# rot=false
+# for pht in 16 32 64 128 256 512 1024 perfect
+# do
+#     srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
+#     dstfile="$(pwd)/src/parameter.rs"
+#     echo "Copying ${srcfile} to ${dstfile}"
+#     cp $srcfile $dstfile
 
-    echo "Building ..."
-    cargo build --release --bin worm_cache
+#     echo "Building ..."
+#     cargo build --release --bin worm_cache
 
-    srcfile="$(pwd)/target/release/worm_cache"
-    dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
-    cp $srcfile $dstfile
-done
+#     srcfile="$(pwd)/target/release/worm_cache"
+#     dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
+#     cp $srcfile $dstfile
+# done
 
-satcnt=false
-rdwr=false
-rot=false
-for pht in 16 32 64 128 256 512 1024 perfect
-do
-    srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
-    dstfile="$(pwd)/src/parameter.rs"
-    echo "Copying ${srcfile} to ${dstfile}"
-    cp $srcfile $dstfile
+# satcnt=true
+# rdwr=false
+# rot=false
+# for pht in 16 32 64 128 256 512 1024 perfect
+# do
+#     srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
+#     dstfile="$(pwd)/src/parameter.rs"
+#     echo "Copying ${srcfile} to ${dstfile}"
+#     cp $srcfile $dstfile
 
-    echo "Building ..."
-    cargo build --release --bin worm_cache
+#     echo "Building ..."
+#     cargo build --release --bin worm_cache
 
-    srcfile="$(pwd)/target/release/worm_cache"
-    dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
-    cp $srcfile $dstfile
-done
+#     srcfile="$(pwd)/target/release/worm_cache"
+#     dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
+#     cp $srcfile $dstfile
+# done
+
+# satcnt=false
+# rdwr=false
+# rot=false
+# for pht in 16 32 64 128 256 512 1024 perfect
+# do
+#     srcfile="$(pwd)/src/configs/parameter_${satcnt}_${rdwr}_${rot}_${pht}.rs"
+#     dstfile="$(pwd)/src/parameter.rs"
+#     echo "Copying ${srcfile} to ${dstfile}"
+#     cp $srcfile $dstfile
+
+#     echo "Building ..."
+#     cargo build --release --bin worm_cache
+
+#     srcfile="$(pwd)/target/release/worm_cache"
+#     dstfile="$(pwd)/../bins/pf/worm_cache_${satcnt}_${rdwr}_${rot}_${pht}"
+#     cp $srcfile $dstfile
+# done
