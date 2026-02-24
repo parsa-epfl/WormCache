6577c301343db6c3b28f6090900bf21090d97b25
ayanchak1508 <ayan.chakraborty@epfl.ch>
2026-02-24 11:03:01 +0100

    chore: cleanup and update scripts

--- scripts/gen_plugin.sh
+++ scripts/gen_plugin.sh
@@ -2,52 +2,33 @@
 
 cd ..
 
-# for type in os an
-# do
-#     for cnt in 1 2 4 8 16 32 64
-#     do
-#         for cache in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576
-#         do
-#             if [ "$type" = "os" ] && [ $cnt -eq 1 ]; then
-#                 continue
-#             fi
-#             srcfile="$(pwd)/src/configs/${type}_${cnt}_${cache}.rs"
-#             dstfile="$(pwd)/src/parameter.rs"
-#             echo "Copying ${srcfile} to ${dstfile}"
-#             cp $srcfile $dstfile
+for type in os an; do
+    for sms in 0 1024 2048 4096 8192 16384; do
+        for stlb in 256 512 1024 2048 4096; do
+            for l1 in 8 16 32 64; do
+                for llc in 1 8 32; do
+                    prefix="SMS_${sms}_STLB_${stlb}_L1_${l1}_LLC_${llc}"
 
-#             echo "Building ..."
-#             cargo build --release
+                    srcfile="$(pwd)/src/configs/single_core/parameter_${type}_${prefix}.rs"
+                    dstfile="$(pwd)/src/parameter.rs"
 
-#             srcfile="$(pwd)/target/release/libworm_cache.so"
-#             dstfile="$(pwd)/../bins/ws/${type}_${cnt}_${cache}.so"
-#             echo "Copying ${srcfile} to ${dstfile}"
-#             cp $srcfile $dstfile
-#         done
-#     done
-# done
+                    echo "Copying ${srcfile} to ${dstfile}"
+                    cp $srcfile $dstfile
 
-satcnt=true
-rdwr=true
-rot=true
+                    echo "Building ..."
+                    cargo build --release
 
-for type in os an
-do
-    for region in 32 64
-    do
-        for pht in 16 32 64 128 256 512 1024
-        do
-            srcfile="$(pwd)/src/configs/parameter_${type}_${region}_${satcnt}_${rdwr}_${rot}_${pht}.rs"
-            dstfile="$(pwd)/src/parameter.rs"
-            echo "Copying ${srcfile} to ${dstfile}"
-            cp $srcfile $dstfile
+                    srcfile="$(pwd)/target/release/libworm_cache.so"
+                    dstfile="$(pwd)/../bins/single_core/libworm_cache_${type}_${prefix}.so"
+                    echo "Copying ${srcfile} to ${dstfile}"
+                    cp $srcfile $dstfile
 
-            echo "Building ..."
-            cargo build --release --bin worm_cache
-
-            srcfile="$(pwd)/target/release/worm_cache"
-            dstfile="$(pwd)/../bins/pf/worm_cache_${type}_${region}_${satcnt}_${rdwr}_${rot}_${pht}"
-            cp $srcfile $dstfile
+                    srcfile="$(pwd)/target/release/checkpoint_conversion"
+                    dstfile="$(pwd)/../bins/single_core/checkpoint_conversion_${type}_${prefix}"
+                    echo "Copying ${srcfile} to ${dstfile}"
+                    cp $srcfile $dstfile
+                done
+            done
         done
     done
 done
