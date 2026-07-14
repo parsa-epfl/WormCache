5fc189d988c9bdb37b4b7056b698a5e686cd20f5
ayanchak1508 <ayan.chakraborty@epfl.ch>
2026-07-14 13:56:49 +0200

    chore: gen scripts

---
+++ scripts/gen_plugin_llc_sweep_4_cores.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd ..
+
+ncores=4
+for tuple in "8192 16384" "2048 4096"; do
+    read -r btb sms <<< "$tuple"
+    for type in os an; do
+        for llc in 256 512 1024 2048 4096 8192 16384; do
+            prefix="BTB_${btb}_SMS_${sms}_STLB_1024_L1_64_LLC_${llc}"
+
+            src_file="$(pwd)/src/cfgs/${ncores}_core/parameter_${type}_${prefix}.rs"
+            dst_file="$(pwd)/src/parameter.rs"
+
+            echo "Copying ${src_file} to ${dst_file}"
+            cp $src_file $dst_file
+
+            echo "Building ..."
+            cargo build --release
+
+            src_file="$(pwd)/target/release/libworm_cache.so"
+            dst_file="$(pwd)/../bins/${ncores}_core/libworm_cache_${type}_${prefix}.so"
+            echo "Copying ${src_file} to ${dst_file}"
+            cp $src_file $dst_file
+
+            src_file="$(pwd)/target/release/checkpoint_conversion"
+            dst_file="$(pwd)/../bins/${ncores}_core/checkpoint_conversion_${type}_${prefix}"
+            echo "Copying ${src_file} to ${dst_file}"
+            cp $src_file $dst_file
+        done
+    done
+done
---
+++ src/scripts/gen_llc_sweep_4_cores.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ncores=4
+for tuple in "8192 16384" "2048 4096"; do
+    read -r btb sms <<< "$tuple"
+    for type in os an; do
+        for llc in 256 512 1024 2048 4096 8192 16384; do
+            ref_prefix="BTB_${btb}_SMS_${sms}_STLB_1024_L1_64_LLC_1024"
+            dst_prefix="BTB_${btb}_SMS_${sms}_STLB_1024_L1_64_LLC_${llc}"
+
+            ref_file="../ref_cfg/${ncores}_core/parameter_${type}_${ref_prefix}.rs"
+            dst_file="../cfgs/${ncores}_core/parameter_${type}_${dst_prefix}.rs"
+            echo "Copying $ref_file to $dst_file"
+
+            cp "$ref_file" "$dst_file"
+
+            sed -i -e "s/SHARED_CACHE_SET: usize = 1024/SHARED_CACHE_SET: usize = ${llc}/g" "$dst_file"
+        done
+    done
+done
